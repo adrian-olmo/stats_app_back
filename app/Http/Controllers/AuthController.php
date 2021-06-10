15451c37c443bf64a2ccad3b9d5efa8d6b3f47c3
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-06-10 19:38:34 +0200

    as

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -103,8 +103,6 @@ class AuthController extends Controller
 
     public function getRole(Request $request)
     {
-        $userLogged = $request->user()->id;
-        $data = Role::where('user_id', $userLogged)->first();
-        return response()->json($data, 200);
+        return $request->user();
     }
 }
